121971b1f5184b4aaf71bd183f9b8504bea7c7ea
happy663 <tatu2425@gmail.com>
2025-11-22 16:39:15 +0900

    update: Enable line breaks in markdown files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import remarkGfm from "remark-gfm";
+import remarkBreaks from "remark-breaks";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    remarkPlugins: [remarkGfm],
+    remarkPlugins: [remarkBreaks],
   },
 });
-
